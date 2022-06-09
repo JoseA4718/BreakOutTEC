f6e453d388482f971eb8dfc1a5ec99fc77493e0b
Jose <joseantonioestudiante2001@gmail.com>
2022-06-08 18:54:20 -0600

    Added generic GSON reader and parser, installed gson as a library

---
+++ Client/src/application/JsonParser/JsonParser.java
@@ -0,0 +1,22 @@
+package application.JsonParser;
+
+import com.google.gson.Gson;
+
+public class JsonParser {
+	
+	public static JsonTestClass deserializeJson(String userJson) {
+		//String usrJson = "{"+ userJson + "}"; 
+
+		Gson gson = new Gson();
+		
+		JsonTestClass json = gson.fromJson(userJson, JsonTestClass.class);
+		
+		if (json.matrix == null) {
+			System.out.println("Llegó nula");
+		}
+		
+		return json;
+		
+	}
+	
+}
---
+++ Client/src/application/JsonParser/JsonTest.java
@@ -0,0 +1,40 @@
+package application.JsonParser;
+
+public class JsonTest {
+	
+	public Integer[][] matrix = new Integer[8][14];
+	public Integer lives;
+	public Integer ballQuantity;
+	public Integer ballSpeed;
+	public Integer[] ballPosition = new Integer[2];
+	public Integer racketLenght;
+	public Integer racketPosition;
+	public Integer level;
+	public Integer score;
+	public Integer greenBrickValue;
+	public Integer yellowBrickValue;
+	public Integer orangeBrickValue;
+	public Integer redBrickValue;
+	
+	public JsonTest(Integer[][] matrix, Integer lives, Integer ballQuantity, Integer ballSpeed,
+			Integer[] ballPosition, Integer racketLenght, Integer racketPosition, Integer level, Integer score,
+			Integer greenBrickValue, Integer yellowBrickValue, Integer orangeBrickValue, Integer redBrickValue) {
+		super();
+		this.matrix = matrix;
+		this.lives = lives;
+		this.ballQuantity = ballQuantity;
+		this.ballSpeed = ballSpeed;
+		this.ballPosition = ballPosition;
+		this.racketLenght = racketLenght;
+		this.racketPosition = racketPosition;
+		this.level = level;
+		this.score = score;
+		this.greenBrickValue = greenBrickValue;
+		this.yellowBrickValue = yellowBrickValue;
+		this.orangeBrickValue = orangeBrickValue;
+		this.redBrickValue = redBrickValue;
+	}
+	
+	
+	
+}
